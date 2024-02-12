693b42e61007a34513481a3abbba03920eeb76bc
Adam Ciuris <adamciuris@gmail.com>
2024-02-12 09:07:22 -0600

    python venv

--- home-manager-module.nix
+++ home-manager-module.nix
@@ -16,8 +16,21 @@ in
 			shellAliases =  { src = "source"; "..." = "../../"; nrs = "sudo nixos-rebuild switch"; 
 								"g*" = "git add *"; "gcm" = "git commit -m";
 								gp = "git push"; # conflicts with global-platform-pro, pari
-								
+								resrc= "source ~/.bashrc";
 								};
+
+			initExtra = ''
+			pvenv() {
+				# starts a python virtual environment named after first arg and if a path to a requirements file is provided as second arg it installs it
+				# "deactivate" leaves the venv
+				local firstArg="$1"
+				local activate="./$firstArg/bin/activate"
+				python -m venv $firstArg && source $activate
+				if [ ! -z "$2" ]; then
+					pip install $2
+				fi
+			}
+			'';
 		};
 		home.stateVersion = "23.11";
 		# programs.home-manager.enable=true;
