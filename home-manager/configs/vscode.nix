8de2d3725b0bfe96df283c2a26753afc7c70fe28
Adam Ciuris <adamciuris@gmail.com>
2024-04-09 03:37:30 -0500

    bash debugger

--- home-manager/configs/vscode.nix
+++ home-manager/configs/vscode.nix
@@ -49,9 +49,11 @@ in
 			ms-python.python
 			shd101wyy.markdown-preview-enhanced
 			ms-toolsai.jupyter
+			
 		]) ++ [
 			(ext "Nix" "bbenoist" "1.0.1" "sha256-qwxqOGublQeVP2qrLF94ndX/Be9oZOn+ZMCFX1yyoH0=") # https://marketplace.visualstudio.com/items?itemName=bbenoist.Nix
 			(ext "copilot" "GitHub"  "1.168.0" "sha256-KoN3elEi8DnF1uIXPi6UbLh+8MCSovXmBFlvJuwAOQg=") # https://marketplace.visualstudio.com/items?itemName=GitHub.copilot
+			(ext  "bash-debug" "rogalmic" "0.3.9" "sha256-f8FUZCvz/PonqQP9RCNbyQLZPnN5Oce0Eezm/hD19Fg=") # https://marketplace.visualstudio.com/items?itemName=rogalmic.bash-debug
 			(ext "nix-ide" "jnoortheen" "0.2.2" "sha256-jwOM+6LnHyCkvhOTVSTUZvgx77jAg6hFCCpBqY8AxIg=" ) # https://marketplace.visualstudio.com/items?itemName=jnoortheen.nix-ide
 		];
 	}; # END VSCODE
