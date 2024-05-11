5ed5d46763daeec19b4b36e3ae303228dcfdfce4
Adam Ciuris <adamciuris@gmail.com>
2024-05-11 10:51:48 -0500

    recurse chmod

--- system/services/openvpn.nix
+++ system/services/openvpn.nix
@@ -2,7 +2,7 @@
 {
   programs.openvpn3.enable = true;
   services.openvpn.servers = {
-    me = {
+    me = { # sudo chmod -R 700  /etc/openvpn
       autoStart = true; # Start the server on boot
       config = '' config /etc/openvpn/server.ovpn '';
     };
