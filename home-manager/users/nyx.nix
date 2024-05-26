b0c5752c4a6a3d12ab214f257fa9af66deb1db9c
Adam Ciuris <adamciuris@gmail.com>
2024-05-26 08:40:03 -0500

    startup apps

--- home-manager/users/nyx.nix
+++ home-manager/users/nyx.nix
@@ -19,9 +19,11 @@
 		stateVersion = "23.11";
 	};
 	home.file = { # starts at ~/.config
-
+		# horrible startup javascript apps
 		".config/autostart/brave-browser.desktop".source = "${pkgs.brave}/share/applications/brave-browser.desktop";
-		".config/autostart/firefox.desktop".source = "${pkgs.firefox}/share/applications/brave-browser.desktop";
+		".config/autostart/firefox.desktop".source = "${pkgs.firefox}/share/applications/firefox.desktop";
+		".config/autostart/codium.desktop".source = "${pkgs.vscodium}/shar/*  */e/applications/codium.desktop";
+		".config/autostart/thunderbird.desktop".source = "${pkgs.thunderbird}/share/applications/thunderbird.desktop";
 	};
 
 
