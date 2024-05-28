62d46fc33c9a512c9546f4715db2609ff0495634
Adam Ciuris <adamciuris@gmail.com>
2024-05-28 10:57:49 -0500

    systemd randomize on boot

--- system/systemd/timers.nix
+++ system/systemd/timers.nix
@@ -22,4 +22,25 @@
       Persistent = true;
     };
   };
+
+    # test with sudo systemctl status randomize.service
+  systemd.services."randomize" = {
+    script = ''
+      ${pkgs.python3.withPackages (python-pkgs: [
+			python-pkgs.pandas
+			python-pkgs.numpy
+		])}/bin/python /home/nyx/Downloads/randumb/randomize.py
+    '';
+    serviceConfig = {
+      # Type = "oneshot";
+      User = "nyx";
+    };
+  };
+  systemd.timers."randomize" = {
+    wantedBy = [ "timers.target" ]; 
+    timerConfig = {
+      OnBootSec = "0";
+      Persistent = true;
+    };
+  };
 }
