5962047ab0ad33720c26d556bf06a56cadbfbeb1
Adam Ciuris <adamciuris@gmail.com>
2024-05-30 12:46:25 -0500

    wallpaper

--- system/specialisations/default-specialisation.nix
+++ system/specialisations/default-specialisation.nix
@@ -3,5 +3,6 @@
 	config = lib.mkIf (config.specialisation != {}) {
 		services.xserver.displayManager.lightdm.enable = true; 
 		services.xserver.desktopManager.cinnamon.enable = true;
+		services.xserver.desktopManager.wallpaper.mode = "centered";
 	};
 })
