d71aff9f98c2ee0676ec50c68832f28efa62299a
Adam Ciuris <adamciuris@gmail.com>
2024-06-02 12:43:41 -0500

    24.05

--- flake.nix
+++ flake.nix
@@ -4,7 +4,7 @@
 
   inputs = {
     # Nixpkgs
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-23.11";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-24.05";
     hardware.url = "github:nixos/nixos-hardware";
 
     # nixos-hardware.url = "github:NixOS/nixos-hardware/master"; # https://github.com/NixOS/nixos-hardware
@@ -12,7 +12,7 @@
 
     # Home manager
     home-manager = {
-      url = "github:nix-community/home-manager/release-23.11";
+      url = "github:nix-community/home-manager/release-24.05";
       inputs.nixpkgs.follows = "nixpkgs";
     };
     nixos-generators = {
