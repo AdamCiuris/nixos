b289b34d3a0fc99f9251c33bbc3a68bd54138aca
Adam Ciuris <adamciuris@gmail.com>
2024-07-09 05:12:41 -0500

    firewall rules, refactor

--- system/networking/network.nix
+++ system/networking/network.nix
@@ -3,6 +3,7 @@
   	networking = 
         {   
             # bind hostname in import
+            firewall.enable = true;
             hostName = "nixos";
             enableIPv6 = false; # ipv4 only pls
             networkmanager.enable = true;
--- system/networking/ports/allOff.nix
+++ system/networking/ports/allOff.nix
@@ -5,5 +5,14 @@
         allowedTCPPorts = lib.mkForce [  ];
         allowedUDPPorts = lib.mkForce [  ];
         logRefusedConnections = true; 
+
+
+      # iptables -P INPUT DROP # stop all incoming traffic
+      # iptables -P FORWARD DROP # stop all forwarding traffic
+      # iptables -P OUTPUT DROP # stop all outgoing traffic
+      # https://www.ibm.com/docs/en/linux-on-systems?topic=tests-firewall-iptables-rules
+      extraCommands = ''
+          iptables -P INPUT DROP
+      '';
 	};
 }
