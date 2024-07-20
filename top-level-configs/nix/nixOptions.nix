d06c2d3fbb9a34bf85e8865ed7733c33cf53431f
Adam Ciuris <adamciuris@gmail.com>
2024-07-20 18:30:05 -0500

    gc 20d

--- top-level-configs/nix/nixOptions.nix
+++ top-level-configs/nix/nixOptions.nix
@@ -1,7 +1,7 @@
 { config, pkgs,  ... }: 
 {
   gc.automatic = true;
-	gc.options = "--delete-older-than 5d";
+	gc.options = "--delete-older-than 20d";
 	settings.experimental-features = ["nix-command" "flakes"]; # needed to try flakes from tutorial
 
 }
