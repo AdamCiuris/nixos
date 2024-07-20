05ab2cda62460f64a13236750fba8a9bfb040836
Adam Ciuris <adamciuris@gmail.com>
2024-07-19 19:22:11 -0500

    portfolio container

---
+++ system/virtualization/portfolio-website.nix
@@ -0,0 +1,41 @@
+{ config, pkgs, ... }:
+let 
+  webPort = 8081;
+in
+{
+  virtualisation.oci-containers = {
+    backend = "docker"; # Use Docker as the backend
+    containers.port-website = {
+      image = "nginx";
+      ports = [ "${builtins.toString webPort}:8081" "8082:8082"];
+      volumes = [ "/home/nyx/GITHUB/nixos/system/virtualization/configs/portfolio-website/:/portfolio-website/" ];
+      # entrypoint = "/usr/sbin/nginx";
+      cmd = ["nginx" "-c" "/portfolio-website/nginx.conf" "-p" "/portfolio-website"];
+      extraOptions = [ 
+        "--add-host=host.docker.internal:host-gateway"
+       ];
+      # restartPolicy = "always";
+    };
+  };
+  networking.firewall.allowedTCPPorts = [ webPort 8082 ];
+  # services.nginx.virtualHosts."ciuris.xyz" = {
+  #   listen= [
+  #     {
+  #       addr="127.0.0.1";
+  #       port = 5454;
+  #       }
+  #     ];
+  #   serverName = "127.0.0.1";
+  #   locations."/".proxyPass = "http://unix:/run/gunicorn.sock";
+  #   locations."/static/".root = "/home/nyx/GITHUB/portfolio-website/";
+  #   # locations."/".proxyPass = "http://unix:/run/gunicorn.sock";
+
+  #     # addSSL = true;
+  #     # enableACME = true;
+  #     # root = "/var/www/myhost.org";
+  # };
+  # security.acme = {
+  #   acceptTerms = true;
+  #   defaults.email = "foo@bar.com";
+  # };
+}
