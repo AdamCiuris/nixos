eca72edef252b26e3d88cc2dd6445b9841986def
Adam Ciuris <adamciuris@gmail.com>
2024-07-22 23:59:25 -0500

    xrdp tests

--- system/services/xrdp.nix
+++ system/services/xrdp.nix
@@ -1,10 +1,21 @@
-{ config, pkgs, ... }: 
+{ config, pkgs, lib, modulesPath, ... }: 
 {
+  imports= [./myxrdp.nix ];
   services.xrdp = {
       enable = true;
-      defaultWindowManager = "startplasma-x11";
-      confDir = /etc/xrdp;
-      port = 8181;
-      openFirewall=false; # https://c-nergy.be/blog/?p=14965/
+      defaultWindowManager = "xfce4-session";
+      confDir = "$out";
+#       extraConfDirCommands= ''
+#   substituteInPlace $out/sesman.ini \
+#     --replace LogLevel=INFO LogLevel=DEBUG \
+#     --replace LogFile=/dev/null LogFile=/var/log/xrdp.log
+# '';
+      port = "tcp://:8181";
+      # openFirewall=true; # https://c-nergy.be/blog/?p=14965/
     };
+    networking.enableIPv6 = lib.mkForce false; # ipv4 only pls
+
+    environment.systemPackages = [
+      pkgs.xrdp
+    ];
 }
