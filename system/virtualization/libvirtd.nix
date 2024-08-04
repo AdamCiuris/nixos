680b9edd0760f671b49672efa7968b5967bbe098
Adam Ciuris <adamciuris@gmail.com>
2024-08-04 18:14:15 -0500

    mount real drives, waow

--- system/virtualization/libvirtd.nix
+++ system/virtualization/libvirtd.nix
@@ -5,3 +5,12 @@
     virt-manager.enable = true;
   };
 }
+# note tags: mapping disks, mounting disks in kvm, mount disk
+# below will mount real sata drive in qemu/kvm stuff
+# https://askubuntu.com/questions/144894/add-physical-disk-to-kvm-virtual-machine
+# <disk type="block" device="disk">
+#   <driver name="qemu" type="raw"/>
+#   <source dev="/dev/sda"/>
+#   <target dev="vda" bus="sata"/>
+#   <address type="drive" controller="0" bus="0" target="0" unit="2"/>
+# </disk>
