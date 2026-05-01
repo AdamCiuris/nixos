cfa23f8b98763b7a7e2cd988ecaeaf2d93143262
Adam Ciuris <adamciuris@gmail.com>
2026-05-01 12:27:29 -0500

    unstable iphone package

--- system/services/iphone.nix
+++ system/services/iphone.nix
@@ -1,6 +1,6 @@
 { config, pkgs, ... }:{
   services.usbmuxd = {
-    package= pkgs.usbmuxd2;
+    package= pkgs.unstable.usbmuxd2;
     user = "usbmux"; # default is "usbmux"
     group = "usbmux";
     enable = true;
