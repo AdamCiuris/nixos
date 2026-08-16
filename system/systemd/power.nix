e3b24324cf5a53181581341378cd00b725373501
Adam Ciuris <adamciuris@gmail.com>
2026-08-15 23:20:08 -0500

    csm

---
+++ system/systemd/power.nix
@@ -0,0 +1,10 @@
+{ config, pkgs, lib, ... }:
+{
+  # Completely disable suspend and hibernation
+  systemd.sleep.settings.Sleep = {
+    AllowSuspend = "no";
+    AllowHibernation = "no";
+    AllowHybridSleep = "no";
+    AllowSuspendThenHibernate = "no";
+  };
+}
