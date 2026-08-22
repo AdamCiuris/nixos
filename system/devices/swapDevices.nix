20113e4643e3c1ebb35ce27a322485bbafa85e80
Adam Ciuris <adamciuris@gmail.com>
2026-08-22 07:42:24 -0500

    zram

--- system/devices/swapDevices.nix
+++ system/devices/swapDevices.nix
@@ -1,13 +1,28 @@
-# https://phoenixnap.com/kb/linux-swap-file
-# https://search.nixos.org/options?channel=24.05&show=swapDevices&from=0&size=50&sort=relevance&type=packages&query=swap
-{ config, pkgs, ... }:
+{ config, lib, pkgs, ... }:
+# https://www.youtube.com/watch?v=RGVt16xiERc
+
 {
-  swapDevices = [
-    {
-      device = "/__swapfile__";
-      size = 1024*16; # in MiB, 32 GiB
-      priority = 10;
-      # label = "swap";
-    }
-  ];
+  # do not ever use a swapfile on low end machines
+  # Remove or comment out the physical swapfile
+  # swapDevices = [ { device = "/__swapfile__"; ... } ];
+
+  # Enable compressed RAM swap
+  zramSwap = {
+    enable = true;
+    memoryPercent = 50; # Creates a zram device up to 50% of your total RAM
+  };
+
+  boot.kernel.sysctl = {
+    # zram is fast; encourage the kernel to use it proactively
+    "vm.swappiness" = 100;
+    
+    # Keep metadata cached
+    "vm.vfs_cache_pressure" = 50;
+    
+    # Wake up the background paging daemon earlier
+    "vm.watermark_scale_factor" = 125;
+    
+    # Optimize page clustering for zram
+    "vm.page-cluster" = 0; 
+  };
 }
